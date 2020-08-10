e4c899bdf77f7f4f8831426da3de5015a1fe9793
Mai201 <64534832+Mai201@users.noreply.github.com>
2020-08-10 22:41:01 +0200

    update panier
    ajout bouton "vider le panier"
    pb données réglé
    tjrs bug affichage tableau

--- js/panier.js
+++ js/panier.js
@@ -15,30 +15,39 @@ promiseGet()
         {
             if (userBasket[f][0] === response[e]._id) 
             {
-                if (chosenItems) 
-                {
-                    chosenItems.innerHTML += `
-                    <td>${response[e].name} </td>
-                    <td>${response[e].price / 100} € </td>
-                    <td>${userBasket[f][2]} </td>
-                    <td>${userBasket[f][1]} </td>
-                    <td>${(response[e].price / 100) * userBasket[f][1]}€ </td>`
+                chosenItems.innerHTML += `
+                <td>${response[e].name} </td>
+                <td>${response[e].price / 100} € </td>
+                <td>${userBasket[f][2]}(NC) </td>
+                <td>${userBasket[f][1]} </td>
+                <td>${(response[e].price / 100) * userBasket[f][1]}€ </td>`
                     
-                    totalPay += (response[e].price / 100) * userBasket[f][1]
-                }
+                totalPay += (response[e].price / 100) * userBasket[f][1]
             }
         }
     }
+
     if (totalPay >= 1) 
     {
-        if (totalContent) 
-        {
-            totalContent.textContent = `Récapitulatif de votre commande d'un total de : ${totalPay}€`
-        } else 
-        {
-            totalContent.textContent = "Vous n'avez pas d'article dans le panier actuellement"
-        }
+        totalContent.textContent = `Récapitulatif de votre commande d'un total de : ${totalPay}€`
+    } else 
+    {
+        totalContent.textContent = "Vous n'avez pas d'article dans le panier actuellement"
     }
+
+    // ajout d'un bouton pour vider la panier
+    let divRemoveArticle = document.querySelector(".removeArticle");
+    divRemoveArticle.classList.add("row");
+    let removeArticle = document.createElement("a");
+    removeArticle.classList.add("btn", "btn-primary", "btn-top");
+    removeArticle.type="button";
+    removeArticle.href="panier.html";
+    removeArticle.classList.add = ""
+    divRemoveArticle.appendChild(removeArticle).innerHTML+="Vider le panier";
+    removeArticle.addEventListener("click", function (e) {
+        localStorage.clear()
+    })
+
 })
 
 .catch(function (error)
